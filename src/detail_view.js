e98759be470ede57d154be0f09f57a808c55618c
Fabian Beliza <fabianbeliza+github@icloud.com>
2020-05-05 10:37:49 +0200

    Update detail_view.js

--- src/detail_view.js
+++ src/detail_view.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { createApolloFetch } from "apollo-fetch";
 import Starship from './starship';
-const uri = "https://api.graphcms.com/simple/v1/swapi";
+const uri = "https://api-euwest.graphcms.com/v1/ck5wca13qc9ux01fgaidt12m4/master";
 const apolloFetch = createApolloFetch({ uri });
 class DetailView extends Component {
   state = {
